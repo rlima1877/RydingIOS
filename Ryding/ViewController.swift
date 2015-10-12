fc62fe528bc1868ce653dcf5d83488f7caac3cc2
David Bethala <david.bethala@outlook.com>
2015-10-12 16:20:46 -0400

    Testing 2

--- Ryding/ViewController.swift
+++ Ryding/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //this comment 
+        //this doesn't do anything 
     }
 
     override func didReceiveMemoryWarning() {
