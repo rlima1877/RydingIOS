dcc26b31803be199f98c8132807007e3dc257b80
David Bethala <david.bethala@icloud.com>
2015-10-12 20:32:07 -0400

    Added SwiftHTTP files for easier handling of requests

---
+++ Ryding/BusMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BusMapViewController.swift
+//  Ryding
+//
+//  Created by David Bethala on 10/12/15.
+//  Copyright © 2015 Rafael Lima. All rights reserved.
+//
+
+import UIKit
+
+class BusMapViewController: UIViewController {
+    
+}
